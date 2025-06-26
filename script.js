d843f72e478448b684badc42502c60d3ebd74f90
bolinhomendess <marcelly.vaz@escola.pr.gov.br>
2025-06-26 11:43:36 +0000

    script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- const
+ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+ botaoMostraPalavras.addEventListener('click,mostraPalavraChave');
+
+ function mostraPalavraChave (){
+   const texto = document.querySelector ('#entrada-de-texto').value;
+   const campoResultado = document.querySelector('#resultado-palavrachave');
+
+   campoResultado.textContent = texto;
+ }
